a468d29bc1543555cccd9ccdff7cd4cbf22ecf0d
licheng58 <928669548@qq.com>
2021-10-26 16:11:31 +0800

    222

--- src/components/TodoHeader.js
+++ src/components/TodoHeader.js
@@ -1,16 +1,56 @@
 import React, { Component } from 'react'
-
-export default class TodoHeader extends Component {
+import { connect } from 'react-redux'
+import { addTodo } from '../redux/action'
+class TodoHeader extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: '',
+    }
+  }
   render() {
     return (
       <header className="header">
-        <h1>todos</h1>
+        <h1>todos111</h1>
         <input
           className="new-todo"
           placeholder="What needs to be done?"
           autoFocus
+          value={this.state.value}
+          onChange={this.handleChange}
+          onKeyUp={this.handleKeyUp}
         />
       </header>
     )
   }
+
+  // 绑定input
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    })
+  }
+
+  handleKeyUp = (e) => {
+    if (e.keyCode === 13) {
+      this.props.dispathAddTodo(this.state.value)
+      this.setState({
+        value: '',
+      })
+    }
+  }
 }
+
+const mapStateToProps = (state) => {
+  return {}
+}
+
+const mapActionToProps = (dispash) => {
+  return {
+    dispathAddTodo(value) {
+      dispash(addTodo(value))
+    },
+  }
+}
+
+export default connect(mapStateToProps, mapActionToProps)(TodoHeader)
--- src/components/TodoMain.js
+++ src/components/TodoMain.js
@@ -1,52 +1,134 @@
-import React, { Component } from 'react'
+import React, { Component, useRef } from 'react'
 import { connect } from 'react-redux'
-import { delTodo } from '../redux/action'
+import { delTodo, checkItem, keyUp2 } from '../redux/action'
+import classNames from 'classnames'
 
 class TodoMain extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      editVal: '',
+    }
+
+    // this.toggleRef = React.createRef()
+  }
+
   render() {
-    // console.log(this.props)
-    const { list, dispatchDel } = this.props
     return (
       <section className="main">
         <input id="toggle-all" className="toggle-all" type="checkbox" />
         <label htmlFor="toggle-all">Mark all as complete</label>
-        <ul className="todo-list">
-          {list.map((item) => {
-            return (
-              <li className="completed" key={item.key}>
-                <div className="view">
-                  <input className="toggle" type="checkbox" defaultChecked />
-                  <label>{item.name}</label>
-                  <button
-                    className="destroy"
-                    onClick={() => dispatchDel(item.id)}
-                  ></button>
-                </div>
-                <input
-                  className="edit"
-                  defaultValue="Create a TodoMVC template"
-                />
-              </li>
-            )
-          })}
-        </ul>
+        <ul className="todo-list">{this.renderItems()}</ul>
       </section>
     )
+  }
+
+  renderItems() {
+    const { list, dispatchDelTodo, dispatchCheck } = this.props
+    // const liRef = useRef()
+    // const liRefList = useRef([])
+    // function getRef(dom) {
+    //   liRefList.current.push(dom)
+    // }
+    // console.log(list)
+    if (list.length === 0) {
+      return (
+        <div
+          style={{ height: '100px', textAlign: 'center', lineHeight: '100px' }}
+        >
+          没有更多数据
+        </div>
+      )
+    } else {
+      return list.map((item, index) => {
+        return (
+          <li
+            className={classNames({ completed: item.done })}
+            key={item.id}
+            onDoubleClick={this.handleDoubelClick.bind(this, index, item.name)}
+          >
+            <div className="view" id={'view' + index}>
+              {/* checkbox选择框 */}
+              <input
+                className="toggle"
+                type="checkbox"
+                checked={item.done}
+                onChange={() => dispatchCheck(item.id)}
+              />
+              <label>{item.name}</label>
+              {/* 删除按钮 */}
+              <button
+                className="destroy"
+                onClick={() => dispatchDelTodo(item.id)}
+              ></button>
+            </div>
+            {/* 修改 */}
+            <input
+              id={'edit' + index}
+              className="edit"
+              value={this.state.editVal}
+              onChange={this.handleEdit}
+              onKeyUp={(e) => this.handleKeyUp2(e, item.id, index)}
+            />
+          </li>
+        )
+      })
+    }
+  }
+
+  // 修改
+  handleEdit = (e) => {
+    this.setState({
+      editVal: e.target.value,
+    })
+  }
+
+  // 修改回车
+  handleKeyUp2 = (e, id, index) => {
+    const { dispatchKeyUp2 } = this.props
+    if (e.keyCode === 13) {
+      dispatchKeyUp2(this.state.editVal, id)
+      const aa = document.getElementById(`view${index}`)
+      const bb = document.getElementById(`edit${index}`)
+      aa.style.display = 'block'
+      bb.style.display = 'none'
+    }
+  }
+
+  // 双击
+  handleDoubelClick = (index, name) => {
+    this.setState({
+      editVal: name,
+    })
+    const aa = document.getElementById(`view${index}`)
+    const bb = document.getElementById(`edit${index}`)
+    aa.style.display = 'none'
+    bb.style.display = 'block'
+    bb.focus()
   }
 }
 
 const mapStateToProps = (state) => {
+  // console.log(state)
   return {
-    list: state
+    list: state,
   }
 }
 
 const mapActionToProps = (dispatch) => {
   return {
-    dispatchDel(id) {
-      const action = delTodo(id)
-      dispatch(action)
-    }
+    dispatchDelTodo(id) {
+      dispatch(delTodo(id))
+    },
+
+    dispatchCheck(id) {
+      dispatch(checkItem(id))
+    },
+
+    dispatchKeyUp2(val, id) {
+      // console.log(this.state.editVal)
+      dispatch(keyUp2(val, id))
+    },
   }
 }
 
